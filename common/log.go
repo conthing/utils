e6ea28a1cd818b051fdd5efc5d4b8cfbb6345325
conthing <account@conthing.com>
2019-06-17 21:31:12 +0800

    zerolog修改

--- common/log.go
+++ common/log.go
@@ -1,21 +1,25 @@
 package common
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 // Log 全局的loggerclient
-var Log = logrus.New()
-var logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Str("app", "znet").Caller().Logger()
+var logger zerolog.Logger
 
 // InitLoggerClient 初始化loggerclient
 func InitLoggerClient(logLevel string, logTarget string, serviceName string, isRemote bool) {
-	fmt.Printf("%s %s %s %v",logLevel,logTarget,serviceName,isRemote)
+	fmt.Printf("%s %s %s %v", logLevel, logTarget, serviceName, isRemote)
 	var file io.Writer
-	if logTarget == ""{
+	if logTarget == "" {
 		file = os.Stderr
-	}else{
+	} else {
 		file, err := os.OpenFile(logTarget, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err == nil {
 			log.Out = file
