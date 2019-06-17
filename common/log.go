9dd21924225374583809a67dcf71e1ab18db930c
conthing <account@conthing.com>
2019-06-17 19:08:18 +0800

    修改log使用logrus

--- common/log.go
+++ common/log.go
@@ -1,13 +1,23 @@
 package common
 
 import (
-	logger "github.com/edgexfoundry/go-mod-core-contracts/clients/logging"
+	"github.com/sirupsen/logrus"
 )
 
 // Log 全局的loggerclient
-var Log logger.LoggingClient
+var Log = logrus.New()
 
 // InitLoggerClient 初始化loggerclient
 func InitLoggerClient(logLevel string, logTarget string, serviceName string, isRemote bool) {
-	Log = logger.NewClient(serviceName, isRemote, logTarget, logLevel)
+	fmt.Printf("%s %s %s %v",logLevel,logTarget,serviceName,isRemote)
+	if logTarget == ""{
+		Log.Out = os.Stdout
+	}else{
+		file, err := os.OpenFile(logTarget, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err == nil {
+			log.Out = file
+		} else {
+			log.Info("Failed to log to file, using default stderr")
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,5 @@
 module github.com/conthing/utils
 
 require (
-	github.com/edgexfoundry/go-mod-core-contracts v0.0.0-20190206111100-148998445693
 	golang.org/x/text v0.3.2
 )
