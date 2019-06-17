e500369747c4874efb4981cfdc40c0e5fbd3d277
conthing <account@conthing.com>
2019-06-17 16:01:01 +0800

    bootstrap简化

--- common/bootstrap.go
+++ common/bootstrap.go
@@ -3,74 +3,28 @@ package common
 import (
 	"fmt"
 	"os"
-	"sync"
 	"time"
 )
 
 // BootFunc Boot函数的原型
-type BootFunc func(param interface{}) error
+type BootFunc func(params interface{}) (err error, dontRetry bool)
 
-// BootstrapError 带是否需要retry信息的error，由BootFunc函数返回
-type BootstrapError struct {
-	DontRetry bool
-	ErrMsg    string
-}
-
-// NewBootstrapError 创建BootstrapError
-func NewBootstrapError(dontRetry bool, err string) error {
-	e := BootstrapError{DontRetry: dontRetry, ErrMsg: err}
-	return e
-}
-
-// Error 标准接口
-func (e BootstrapError) Error() string {
-	return e.ErrMsg
-}
-
-// Bootstrap 调用入口
-func Bootstrap(params interface{}, boot BootFunc, timeout int) {
-	deps := make(chan error, 2)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go retry(params, boot, timeout, &wg, deps)
-	go func(ch chan error) {
-		for {
-			select {
-			case e, ok := <-ch:
-				if ok {
-					fmt.Fprintf(os.Stderr, "Boot error:%v\n", e)
-				} else {
-					return
-				}
-			}
-		}
-	}(deps)
-
-	wg.Wait()
-}
-
-func retry(params interface{}, boot BootFunc, timeout int, wait *sync.WaitGroup, ch chan error) {
+// Bootstrap 调用入口，timeout和retry单位ms
+func Bootstrap(boot BootFunc, params interface{}, timeout int, interval int) (err error) {
 	until := time.Now().Add(time.Millisecond * time.Duration(timeout))
 	for time.Now().Before(until) {
-		var err error
-
-		err = boot(params)
+		err, dontRetry := boot(params)
 		if err != nil {
-			ch <- err
-			if bootstrapErr, ok := err.(BootstrapError); ok {
-				if bootstrapErr.DontRetry {
-					// no need to retry, break
-					break
-				}
+			fmt.Fprintf(os.Stderr, "BOOTERR: %v\n", err)
+			if dontRetry {
+				// no need to retry, break
+				break
 			}
 		} else {
 			// success, break
 			break
 		}
-		time.Sleep(time.Second * time.Duration(1))
+		time.Sleep(time.Millisecond * time.Duration(interval))
 	}
-	close(ch)
-	wait.Done()
-
 	return
 }
